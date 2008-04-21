374cc404c3962d6b1bfa67cc63c525c6c9996bb5
lordgraga <lordgraga@7d58e374-d54a-0410-955b-4729bd276e84>
2008-04-21 14:14:53 +0000

    Basic add/remove functions added to Activity, Schedule, Week. Tied them up as loosely as possible.

--- Bedmakers who doesn't sheet/src/activity/Activity.java
+++ Bedmakers who doesn't sheet/src/activity/Activity.java
@@ -1,6 +1,9 @@
 package activity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import schedule.Week;
 
 import employee.Employee;
 
@@ -9,12 +12,29 @@ public class Activity {
 	enum states{NOT_STARTED,IN_PROGRESS,COMPLETED};
 	
 	String name;
-	Project partOfProject;
-	int startWeek;
-	int endWeek;
-	float workHours;
+	Project parentProject;
+	HashMap<Week,Float> weeks;
 	String description;
 	ArrayList<Employee> assignedEmployees;
-	int state;
-	int completion;
+	//Ubrugte felter.
+	//int startWeek;
+	//int endWeek;
+	//int state;
+	//int completion;
+	
+	public void remove() {
+		for(Week week : weeks.keySet()) week.removeActivity(this);
+	}
+
+	public void addWeek(Week week, float hours) {
+		weeks.put(week,hours);
+	}
+
+	public void removeWeek(Week week) {
+		weeks.remove(week);
+	}
+
+	public float getHoursForWeek(Week week) {
+		return weeks.get(week);
+	}
 }
