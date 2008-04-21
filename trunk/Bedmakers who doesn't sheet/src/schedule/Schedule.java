48df93da3dbf6e2d1c3973093838a197392f3b6a
lordgraga <lordgraga@7d58e374-d54a-0410-955b-4729bd276e84>
2008-04-21 14:14:53 +0000

    Basic add/remove functions added to Activity, Schedule, Week. Tied them up as loosely as possible.

--- trunk/Bedmakers who doesn't sheet/src/schedule/Schedule.java
+++ trunk/Bedmakers who doesn't sheet/src/schedule/Schedule.java
@@ -2,8 +2,37 @@ package schedule;
 
 import java.util.ArrayList;
 
-public class Schedule {
-	
-	ArrayList<Week> weeks;
+import activity.Activity;
 
+public class Schedule {
+	ArrayList<Week> weeks;
+	public Schedule() { //Parm: Start date?
+		weeks = new ArrayList<Week>();
+	}
+	
+	public void assignActivityToWeek(Activity activity, int hours, Week week) {
+		activity.addWeek(week, hours);
+		week.addActivity(activity);
+	}
+	
+	public void assignActivityToWeek(Activity activity, int hours, int weekIndex) {
+		assignActivityToWeek(activity, hours, getWeek(weekIndex));
+	}
+	
+	public void removeActivityFromWeek(Activity activity, Week week) {
+		activity.removeWeek(week);
+		week.removeActivity(activity);
+	}
+	
+	public void removeActivity(Activity activity) {
+		activity.remove();
+	}
+	
+	public void removeActivityFromWeek(Activity activity, int weekIndex) {
+		removeActivityFromWeek(activity, getWeek(weekIndex));
+	}
+	
+	Week getWeek(int index) {
+		return weeks.get(index);
+	}
 }
--- trunk/Bedmakers who doesn't sheet/src/schedule/Week.java
+++ trunk/Bedmakers who doesn't sheet/src/schedule/Week.java
@@ -9,5 +9,17 @@ public class Week {
 	
 	ArrayList<Activity> scheduledActivities;
 	int availableHours;
-
+	public void addActivity(Activity activity) {
+		scheduledActivities.add(activity);
+	}
+	public void removeActivity(Activity activity) {
+		scheduledActivities.remove(activity);
+	}
+	public float getAssignedHours() {
+		float hoursum = 0;
+		for(Activity activity : scheduledActivities) {
+			hoursum += activity.getHoursForWeek(this);
+		}
+		return hoursum;
+	}
 }
