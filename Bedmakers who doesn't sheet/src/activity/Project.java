5697b1500a81e70457d3150b78bcb4192098f175
lordgraga <lordgraga@7d58e374-d54a-0410-955b-4729bd276e84>
2008-04-26 18:45:21 +0000

    Class functinality expansions

--- Bedmakers who doesn't sheet/src/activity/Project.java
+++ Bedmakers who doesn't sheet/src/activity/Project.java
@@ -9,8 +9,28 @@ public class Project {
 	ArrayList<Activity> activities;
 	Employee leader;
 	
+	public Project(String name, Employee leader) {
+		this.name = name;
+		this.leader = leader;
+	}
+	
+	public Project(String name) {
+		Project(name, null);
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+	
 	void assignLeader(Employee leader)  {
 		this.leader = leader;
 	}
 	
+	void addActivity(Activity activity) {
+		activities.add(activity);
+	}
+
+	void removeActivity(Activity activity) {
+		activities.add(activity);
+	}
 }
