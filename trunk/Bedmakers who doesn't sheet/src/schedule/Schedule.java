de698cefdf6462e352bee70bc38016e918594a75
lordgraga <lordgraga@7d58e374-d54a-0410-955b-4729bd276e84>
2008-04-26 18:45:21 +0000

    Class functinality expansions

--- trunk/Bedmakers who doesn't sheet/src/schedule/Schedule.java
+++ trunk/Bedmakers who doesn't sheet/src/schedule/Schedule.java
@@ -6,32 +6,38 @@ import activity.Activity;
 
 public class Schedule {
 	ArrayList<Week> weeks;
-	public Schedule() { //Parm: Start date?
-		weeks = new ArrayList<Week>();
+
+	public Schedule(int length) { //CON: Start date?
+		weeks = new ArrayList<Week>(length);
 	}
-	
+
+	public Schedule() {
+		weeks = new ArrayList<Week>(20 * 52); // FIXME: Allocate dynamically
+	}
+
 	public void assignActivityToWeek(Activity activity, int hours, Week week) {
 		activity.addWeek(week, hours);
 		week.addActivity(activity);
 	}
-	
+
 	public void assignActivityToWeek(Activity activity, int hours, int weekIndex) {
 		assignActivityToWeek(activity, hours, getWeek(weekIndex));
 	}
-	
+
 	public void removeActivityFromWeek(Activity activity, Week week) {
 		activity.removeWeek(week);
 		week.removeActivity(activity);
 	}
-	
+
 	public void removeActivity(Activity activity) {
 		activity.remove();
 	}
-	
+
 	public void removeActivityFromWeek(Activity activity, int weekIndex) {
 		removeActivityFromWeek(activity, getWeek(weekIndex));
 	}
-	
+
+	//CON: Make public?
 	Week getWeek(int index) {
 		return weeks.get(index);
 	}
