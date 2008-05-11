e4ccfed50906f12c98558aa264aaf0ca2abdb29a
lordgraga <lordgraga@7d58e374-d54a-0410-955b-4729bd276e84>
2008-05-11 11:11:02 +0000

    getActivityXXX - infoting.

--- Bedmakers who doesn't sheet/src/projectplanner/Activity.java
+++ Bedmakers who doesn't sheet/src/projectplanner/Activity.java
@@ -3,26 +3,16 @@ package projectplanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-//TODO: Frozen exceptions
-
 public class Activity {
 	private String name;
 	private Project parentProject;
-	private HashMap<Week, Float> weeks;
-	//private String description;
+	private HashMap<Week, Float> weeklyWorkload;
 	private ArrayList<Employee> assignedEmployees;
 	private ArrayList<Employee> assistants;
-	//private float workload;
 	private HashMap<Employee, Float> progressByEmployee;
 	private final String id;
 	private boolean frozen;
 	private boolean hasparent;
-
-	// enum states{NOT_STARTED,IN_PROGRESS,COMPLETED};
-	// int startWeek;
-	// int endWeek;
-	// int state;
-	// int completion;
 
 	public Activity(String id, String name) {
 		hasparent = false;
@@ -30,7 +20,7 @@ public class Activity {
 		frozen = false;
 		this.name = name;
 		this.parentProject = null;
-		weeks = new HashMap<Week, Float>();
+		weeklyWorkload = new HashMap<Week, Float>();
 		assistants = new ArrayList<Employee>();
 		assignedEmployees = new ArrayList<Employee>();
 	}
@@ -41,7 +31,7 @@ public class Activity {
 		frozen = false;
 		this.name = name;
 		this.parentProject = parent;
-		weeks = new HashMap<Week, Float>();
+		weeklyWorkload = new HashMap<Week, Float>();
 		assistants = new ArrayList<Employee>();
 		assignedEmployees = new ArrayList<Employee>();
 		progressByEmployee = new HashMap<Employee, Float>();
@@ -57,7 +47,7 @@ public class Activity {
 		checkFreeze();
 		this.name = newName;
 	}
-
+	
 	public void freeze() throws FrozenException {
 		checkFreeze();
 		frozen = true;
@@ -107,23 +97,20 @@ public class Activity {
 		return assistants;
 	}
 
-	// public void remove() {
-	// parentProject.removeActivity(this);
-	// for(Week week : weeks.keySet()) week.removeActivity(this);
-	// }
-
 	public void addWeek(Week week, float hours) throws FrozenException {
 		checkFreeze();
-		weeks.put(week, hours);
+		weeklyWorkload.put(week, hours);
 	}
 
 	public void removeWeek(Week week) throws FrozenException {
 		checkFreeze();
-		weeks.remove(week);
+		weeklyWorkload.remove(week);
 	}
 
 	public float getHoursForWeek(Week week) {
-		return weeks.get(week); // FIXME: Hvad hvis ugen ikke er i array'et?
+		 // FIXME: Hurtigt fix, burde throw exception
+		if(weeklyWorkload.containsKey(week)) return 0;
+		return weeklyWorkload.get(week);
 	}
 
 	public void removeAssistant(Employee employee) throws FrozenException {
@@ -143,9 +130,13 @@ public class Activity {
 		return progress;
 	}
 	
+	public HashMap<Employee,Float> getProgressByEmployee() {
+		return progressByEmployee;
+	}
+	
 	public float getWorkload() {
 		float workload = 0;
-		for (Week w : weeks.keySet())
+		for (Week w : weeklyWorkload.keySet())
 			workload += getHoursForWeek(w);
 		return workload;
 	}
